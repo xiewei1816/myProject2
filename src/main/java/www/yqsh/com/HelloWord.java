d2873805d7391f7f7bf0a527a71b2491bc8ad22b
xiewei1816 <905088157@qq.com>
2019-01-17 11:33:48 +0800

    asd

--- src/main/java/www/yqsh/com/HelloWord.java
+++ src/main/java/www/yqsh/com/HelloWord.java
@@ -7,6 +7,6 @@ package www.yqsh.com;
  */
 public class HelloWord {
     public static void main(String[] args) {
-        System.out.println("Hello Word");
+        System.out.println("Hello Word!");
     }
 }
